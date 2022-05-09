58ef9dcd07a5e85307aa2b399fbd5ba6179d7b02
zafar143 <zfrstn001@gmail.com>
2022-05-09 11:29:30 +0000

    first change java code

---
+++ src/main/java/com/zafardemo/app/App.java
@@ -0,0 +1,13 @@
+package com.zafardemo.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!!!! Learning java from aws" );
+    }
+}
